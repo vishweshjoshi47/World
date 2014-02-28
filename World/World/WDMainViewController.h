053e57477d55f20976cd5b517e2d74935729a2b6
Vishwesh K Joshi <vishwesh.joshi@robosoftin.com>
2014-02-28 14:46:21 +0530

    Commit 2.

---
+++ World/World/WDMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  WDMainViewController.h
+//  World
+//
+//  Created by Vishwesh K Joshi on 28/02/14.
+//  Copyright (c) 2014 Vishwesh K Joshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WDMainViewController : UICollectionViewController
+
+@end
---
+++ World/World/WDMainViewController.m
@@ -0,0 +1,38 @@
+//
+//  WDMainViewController.m
+//  World
+//
+//  Created by Vishwesh K Joshi on 28/02/14.
+//  Copyright (c) 2014 Vishwesh K Joshi. All rights reserved.
+//
+
+#import "WDMainViewController.h"
+
+@interface WDMainViewController ()
+
+@end
+
+@implementation WDMainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        NSLog(@"First View Controller");
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
