dab545fb5acc6a26002d3e48a5ec549dbaa9dd38
Vishwesh K Joshi <vishwesh.joshi@robosoftin.com>
2014-02-28 14:43:07 +0530

    Initial Commit

---
+++ World/World/WDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WDAppDelegate.h
+//  World
+//
+//  Created by Vishwesh K Joshi on 28/02/14.
+//  Copyright (c) 2014 Vishwesh K Joshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ World/World/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  World
+//
+//  Created by Vishwesh K Joshi on 28/02/14.
+//  Copyright (c) 2014 Vishwesh K Joshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WDAppDelegate class]));
+    }
+}
